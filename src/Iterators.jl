c2d11f63b6193328f0e8a6539e7194118663b246
Simon Kornblith <simon@simonster.com>
2013-05-28 23:48:09 -0400

    Add map iterator

--- src/Iterators.jl
+++ src/Iterators.jl
@@ -14,7 +14,8 @@ export
     chain,
     product,
     distinct,
-    partition
+    partition,
+    mapiterator
 
 
 # Infinite counting
@@ -331,6 +332,25 @@ function next(it::Partition, state)
 end
 
 done(it::Partition, state) = done(it.xs, state[1])
+
+
+# Apply a function to each value yielded
+
+immutable MapIterator{I}
+    f::Function
+    xs::I
+end
+
+mapiterator(f::Function, xs) = MapIterator(f, xs)
+
+start(it::MapIterator) = start(it.xs)
+
+function next(it::MapIterator, state)
+    x, state = next(it.xs, state)
+    (it.f(x), state)
+end
+
+done(it::MapIterator, state) = done(it.xs, state)
 
 end # module Iterators
 
