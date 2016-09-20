f879c55485bc19e4c30781d1cb073ca9c32841fa
Eric Davies <iamed2@gmail.com>
2016-09-20 14:44:27 -0500

    Improve coverage

--- test/runtests.jl
+++ test/runtests.jl
@@ -87,6 +87,7 @@ end
 # -----
 
 @test collect(chain(1:2:5, 0.2:0.1:1.6)) == [1:2:5; 0.2:0.1:1.6]
+@test collect(chain(1:0, 1:2:5, 0.2:0.1:1.6)) == [1:2:5; 0.2:0.1:1.6]
 
 # product
 # -------
@@ -110,6 +111,9 @@ x = [5, 2, 2, 1, 2, 1, 1, 2, 4, 2]
 @test collect(partition(take(countfrom(1), 6), 2)) == [(1,2), (3,4), (5,6)]
 @test collect(partition(take(countfrom(1), 4), 2, 1)) == [(1,2), (2,3), (3,4)]
 @test collect(partition(take(countfrom(1), 8), 2, 3)) == [(1,2), (4,5), (7,8)]
+@test collect(partition(take(countfrom(1), 0), 2, 1)) == []
+
+@test_throws ArgumentError partition(take(countfrom(1), 8), 2, 0)
 
 # imap
 # ----
